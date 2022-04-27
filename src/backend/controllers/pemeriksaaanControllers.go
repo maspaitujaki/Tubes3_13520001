a09bc27194b327c05453e37fbda179d399a2f7a0
griendy15 <mfajar801@gmail.com>
2022-04-27 13:37:27 +0700

    add kmp and edit create pemeriksaan

--- src/backend/controllers/pemeriksaaanControllers.go
+++ src/backend/controllers/pemeriksaaanControllers.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"dna-matching-api/database"
 	"dna-matching-api/entity"
+	"dna-matching-api/stringMatching"
 	"encoding/json"
 	"io/ioutil"
 	"time"
@@ -20,52 +21,6 @@ func GetAllPemeriksaan(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(pemeriksaans)
 }
 
-func KmpMatch(text string, pattern string) int {
-	var n int = len(text)
-	var m int = len(pattern)
-	var fail []int = ComputeFail(pattern)
-
-	var i int = 0
-	var j int = 0
-
-	for i < n {
-		if text[i] == pattern[j] {
-			if j == m-1 {
-				return i - m + 1
-			}
-			i++
-			j++
-		} else if j > 0 {
-			j = fail[j-1]
-		} else {
-			i++
-		}
-	}
-	return -1
-}
-
-func ComputeFail(pattern string) []int {
-	var m int = len(pattern)
-	var fail []int = make([]int, m)
-	fail[0] = 0
-
-	var j int = 0
-	var i int = 1
-	for x := 1; x < m; x++ {
-		if pattern[i] == pattern[j] {
-			fail[i] = j + 1
-			i++
-			j++
-		} else if j > 0 {
-			j = fail[j-1]
-		} else {
-			fail[i] = 0
-			i++
-		}
-	}
-	return fail
-}
-
 // CreatePemeriksaan creates pemeriksaan
 func CreatePemeriksaan(w http.ResponseWriter, r *http.Request) {
 	// TODO : Implement CreatePemeriksaan dengan KMP dan Boyer-Moore
@@ -75,12 +30,16 @@ func CreatePemeriksaan(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: Cek penyakit ada nggak
 	var penyakit entity.Penyakit
-	database.Connector.Where("nama = ?", pemeriksaan.Penyakit).First(&penyakit)
+	key := database.Connector.Where("nama = ?", pemeriksaan.Penyakit).First(&penyakit)
 
+	if key.Error != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	// // kalo ada lanjut ke bawah
 
 	//TODO : Pemeriksaan rantai
-	var cek int = KmpMatch(pemeriksaan.Rantai, penyakit.Rantai)
+	var cek int = stringMatching.KmpMatch(pemeriksaan.Rantai, penyakit.Rantai)
 	if cek == -1 {
 		pemeriksaan.Hasil = false
 		pemeriksaan.Tanggal = time.Now()
@@ -88,11 +47,6 @@ func CreatePemeriksaan(w http.ResponseWriter, r *http.Request) {
 		pemeriksaan.Hasil = true
 		pemeriksaan.Tanggal = time.Now()
 	}
-
-	// TODO : Assign Hasil dan tanggal()
-	// pemeriksaan.Hasil =
-	// pemeriksaan.Tanggal = time.Now()
-
 	// Penambahan ke database
 	if result := database.Connector.Create(&pemeriksaan); result.Error != nil {
 		w.WriteHeader(http.StatusInternalServerError)
